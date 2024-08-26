f9bf5d7c03198d9b2d4e3b9934931ee900ba721b
Salah Hossam <salah3hossam@gmail.com>
2024-08-26 05:53:54 +0300

    first commit

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function useFetch(api) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("/api/games", {
+          method: "POST",
+          headers: {
+            "Client-ID": import.meta.env.VITE_TWITCH_CLIENT_ID,
+            Authorization: `Bearer ${import.meta.env.VITE_TWITCH_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          body: `fields *, cover.url,videos; 
+                ${api}
+                limit 20;`,
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(`API Error: ${errorData.message}`);
+        }
+
+        const json = await response.json();
+        setLoading(false);
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchGames();
+  }, [api]);
+
+  return { data, loading };
+}
+
+export default useFetch;
